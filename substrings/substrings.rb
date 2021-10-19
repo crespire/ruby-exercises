3158441e8b6157d7dcf433c0bca200d016390585
crespire <li.simmon@gmail.com>
2021-10-19 11:15:38 -0400

    Added sample tests into script

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -12,3 +12,7 @@ def substrings(input, dictionary)
         end
     end
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
