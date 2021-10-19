f958e8a44942588624888690e2c047cbcf6ad792
crespire <li.simmon@gmail.com>
2021-10-19 15:39:13 -0400

    Add special case test of no profit

--- stockpicker/picker.rb
+++ stockpicker/picker.rb
@@ -16,7 +16,7 @@ def stock_picker(values)
             best_profit = profit
         end
     end
-    [buy, sell]
+    [buy, sell] unless best_profit == 0
 end
 
 priceA = [17,3,6,9,15,8,6,1,10]
@@ -25,3 +25,9 @@ priceB = [26,3,6,2,15,8,6,22,1]
 p stock_picker(priceB) #Should return [3, 7]
 priceC = [44, 30, 24, 32, 35, 30, 40, 38, 15]
 p stock_picker(priceC) #Should return [2, 6]
+priceD = [55,1,5,2,5,3,7,6]
+p stock_picker(priceD) #Should return [1, 6]
+priceE = [40,20,10,11,4]
+p stock_picker(priceE) #Should return [2, 3]
+priceF = [40,20,10,6,4]
+p stock_picker(priceF) #Should return nil
