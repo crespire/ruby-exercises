91d342105a482fd7ec509a1f85d11f34a2d3c52c
crespire <li.simmon@gmail.com>
2021-10-19 11:03:16 -0400

    Clean up comments, and update hash to use string keys instead of symbols

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,19 +1,14 @@
-require 'pry-byebug'
-
 def substrings(input, dictionary)
     return nil if input.empty?
     return nil if dictionary.empty?
 
     sanitized = input.downcase.gsub(/[[:punct:]]/, '').split
-    p sanitized
-    
-    # Reduce is only matching each word in the dictionary once, and not for each instance of the word.
-    # Maybe within the reduce we can check each word of the sanitized input.
+        
     dictionary.reduce(Hash.new(0)) do |found, word|
-        p word
-        if sanitized.include? word then            
-            found[word.to_sym] += 1
+        sanitized.each do |match|
+            if match.include? word then
+                found[word] += 1
+            end
         end
-        p found
     end
 end
