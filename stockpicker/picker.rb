20d275c431b26562dbafedd19f92874821ed5dc9
crespire <li.simmon@gmail.com>
2021-10-19 13:00:42 -0400

    Refactor code to use only one loop

--- stockpicker/picker.rb
+++ stockpicker/picker.rb
@@ -3,16 +3,36 @@ def stock_picker(values)
     best_profit = 0
 
     values.each_with_index do |price, index|
-        compare = values.slice(index + 1, values.length-index)
-        compare.each do |future_price|
-            profit = future_price - price
-            if profit > best_profit then
-                best_profit = profit
-                answer = [values.find_index(price), values.find_index(future_price)]
-            end
-        end
+        # Is today's price lower than yesterdays?
+        #   yes, set the buy price to current price
+        #   no, keep yesterday's buy price
+        # Set the sell price to tomorrow's price
+        # Check the profit
+        # Is this profit better than my old profit?
+        #   Yes, then save this sell price
+        #   No, then keep the old sell price
+        # find the indicies in value of the buy and sell prices and return them.
+        buy = price if buy == nil
+        if buy > price then buy = price
+        
+        sell = price if sell == nil
+        if sell < values[index+1] then sell = price
+
+        profit = sell - buy
+        if profit > best_profit then
+            best_profit = profit
+        
+
+        #compare = values.slice(index + 1, values.length-index)
+        #compare.each do |future_price|
+        #    profit = future_price - price
+        #    if profit > best_profit then
+        #        best_profit = profit
+        #        answer = [values.find_index(price), values.find_index(future_price)]
+        #    end
+        #end
     end
-    answer
+    answer = [values.find_index(buy), values.find_index(sell)]
 end
 
 priceA = [17,3,6,9,15,8,6,1,10]
