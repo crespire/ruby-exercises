abd5aa7501f5ff64d78791f9956d26bd9d0c2992
crespire <li.simmon@gmail.com>
2021-10-19 10:57:45 -0400

    Update matching logic to check each word of sanitize

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,12 +1,19 @@
+require 'pry-byebug'
+
 def substrings(input, dictionary)
     return nil if input.empty?
     return nil if dictionary.empty?
 
-    sanitized = input.downcase.gsub(/[[:punct:]]/, '')
+    sanitized = input.downcase.gsub(/[[:punct:]]/, '').split
+    p sanitized
     
+    # Reduce is only matching each word in the dictionary once, and not for each instance of the word.
+    # Maybe within the reduce we can check each word of the sanitized input.
     dictionary.reduce(Hash.new(0)) do |found, word|
-        if sanitized.include? word
+        p word
+        if sanitized.include? word then            
             found[word.to_sym] += 1
         end
+        p found
     end
 end
