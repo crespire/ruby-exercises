f9e305154936899a209b2b4070c5071777da503f
crespire <li.simmon@gmail.com>
2021-10-19 16:04:02 -0400

    Add psuedocode for BubbleSort

---
+++ bubblesort/bsort.rb
@@ -0,0 +1,7 @@
+def bubble(array)
+    # Define swapped Boolean
+    # For each element with index
+    #  If next is out of range, skip
+    #  Compare current with next, if current is larger, swap with next and set swapped to true
+    #  If swapped is false then return, because we're done
+end
