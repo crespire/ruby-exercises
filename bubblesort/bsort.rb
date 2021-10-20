3c4e1fc68835142c21d6acd08cd2803351de002b
crespire <li.simmon@gmail.com>
2021-10-19 20:54:32 -0400

    Cleaned up code using rubocop

--- bubblesort/bsort.rb
+++ bubblesort/bsort.rb
@@ -1,27 +1,30 @@
-def bubble_sort(array)
-    counter = array.length
-    loop do
-        swapped = false
-        array.each_with_index do | element, index |
-            break if index == counter - 1
+# frozen_string_literal: true
 
-            next_val = array[index+1]
-            if element > next_val then
-                swapped = true
-                array[index], array[index+1] = next_val, element
-            end
-        end
-        counter -= 1
-        break if !swapped || counter == 0
+def bubble_sort(array)
+  counter = array.length
+  loop do
+    swapped = false
+    array.each_with_index do |element, index|
+      break if index == counter - 1
+
+      next_val = array[index + 1]
+      if element > next_val
+        swapped = true
+        array[index] = next_val
+        array[index + 1] = element
+      end
     end
-    array
+    counter -= 1
+    break if !swapped || counter.zero?
+  end
+  array
 end
 
-def rand_array(x, max)
-    x.times.map{ Random.rand(max) }
+def rand_array(ele, max)
+  ele.times.map { Random.rand(max) }
 end
 
 10.times do
-    to_sort = rand_array(Random.rand(30), 30 + Random.rand(500))
-    p bubble_sort(to_sort) == to_sort.sort
+  to_sort = rand_array(Random.rand(30), Random.rand(30..500))
+  p bubble_sort(to_sort) == to_sort.sort
 end
